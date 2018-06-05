9382551ed902abf2522a727b16f37e8ee6941e62
Seyed Amjad Seyedi <amjadseyedi@gmail.com>
2018-06-05 22:20:06 +0430

    Add files via upload

---
+++ DPC_DLP.m
@@ -0,0 +1,25 @@
+clear;clc
+load dataset/CMC
+[X0,T,n,d]=readData(CMC);
+c=size(unique(T),1);
+%________________________________________________________________Parameters
+dis=pdist2(X0,X0);
+mu=1;
+sigma=calculateSigma(X0,0.4);
+W=calculateP(X0,dis,mu,sigma);
+p=3/n;
+k2=fix(p*n);
+t2=8;
+alfa=0.01;
+lambda=0;
+%______________________________________________________________Density Peak
+[peaks,nPeak,k1,neighbs,rho]=densityPeaks(X0,dis,n,p,c);
+%________________________________________Provide Data For Label Propagation
+[Y0,Labeled]=labeling(peaks,nPeak,n,neighbs(:,1:k1));
+Y=Y0;
+%_____________________________________________________________________Graph
+[PP,WW]=calculatePP(W,neighbs(:,1:10));
+P=PP;
+%_______________________________________________________________________DLP
+[Labels,t]=DLP(X0,Y0,Labeled,P,PP,alfa,lambda,t2);
+%scatter(X0(:,1),X0(:,2),30,DLP_Label,'filled');
