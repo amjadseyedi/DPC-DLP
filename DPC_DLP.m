62410bac4c21badabed519473e587f2d031cfe1c
Seyed Amjad Seyedi <amjadseyedi@gmail.com>
2018-06-05 22:21:34 +0430

    Update DPC_DLP.m

--- DPC_DLP.m
+++ DPC_DLP.m
@@ -7,7 +7,7 @@ dis=pdist2(X0,X0);
 mu=1;
 sigma=calculateSigma(X0,0.4);
 W=calculateP(X0,dis,mu,sigma);
-p=3/n;
+p=3;
 k2=fix(p*n);
 t2=8;
 alfa=0.01;
@@ -22,4 +22,4 @@ Y=Y0;
 P=PP;
 %_______________________________________________________________________DLP
 [Labels,t]=DLP(X0,Y0,Labeled,P,PP,alfa,lambda,t2);
-%scatter(X0(:,1),X0(:,2),30,DLP_Label,'filled');
+scatter(X0(:,1),X0(:,2),30,Labels,'filled');
